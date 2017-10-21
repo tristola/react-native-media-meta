8fbb5c5a2bb3a28a8c2c8efea11256254d8cc43d
Toni Ristola <toni.ristola@gofore.com>
2017-10-21 09:39:08 +0300

    Method no longer override

--- android/src/main/java/com/mybigday/rnmediameta/RNMediaMetaPackage.java
+++ android/src/main/java/com/mybigday/rnmediameta/RNMediaMetaPackage.java
@@ -18,7 +18,7 @@ public class RNMediaMetaPackage implements ReactPackage {
     return modules;
   }
 
-  @Override
+  // @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
